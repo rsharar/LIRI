0c7d05564dfb77f7c458bc834a6c266613f39079
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-10-12 21:51:15 -0700

    functional spotify request and response

--- liri.js
+++ liri.js
@@ -1,2 +1,21 @@
+// Require the .env file with access tokens
 require("dotenv").config();
+
+
+// ---- KEYS -----
+var keys = require("./keys.js");
+
+// ---- SPOTIFY -----
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+
+// npm spotify documentation example
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log(data); 
+  });
+
+  
